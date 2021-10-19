99e5ae9a2260216e2228117fd34c64ca877e6a2b
Shadab Khan <shadab2207@gmail.com>
2021-10-19 16:48:20 +0530

    tool button code added

--- public/animation.js
+++ public/animation.js
@@ -3,7 +3,7 @@ let toolbox = document.querySelector(".tool-box");
 let menuOption = document.querySelector("#menu-option");
 let menuClose = document.querySelector("#menu-close")
 
-toolButton.addEventListener("click", function(){
+/* toolButton.addEventListener("click", function(){
     
     if(toolbox.classList.contains("hide")){
         toolbox.classList.remove("hide");
@@ -14,5 +14,20 @@ toolButton.addEventListener("click", function(){
         
     }
 
+
+}) */
+
+cross.addEventListener("click", function(){
+    toolbox.classList.add("hide");
+    cross.classList.add("hide");
+    hamOption.classList.remove("hide");
+
+})
+
+hamOption.addEventListener("click", function(){
+    toolbox.classList.remove("hide");
+    hamOption.classList.add("hide");
+    cross.classList.remove("hide");
+    
 
 })
